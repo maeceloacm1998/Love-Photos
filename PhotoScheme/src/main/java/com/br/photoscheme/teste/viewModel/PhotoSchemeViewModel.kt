db8e6d59462b5952e1683702c73bae3d758198ae
marcelo.mendes <marcelo.cmendes@b2wdigital.com>
2023-01-19 01:05:21 -0300

    feat: add Coroutine to request all photos and update photo

--- PhotoScheme/src/main/java/com/br/photoscheme/teste/viewModel/PhotoSchemeViewModel.kt
+++ PhotoScheme/src/main/java/com/br/photoscheme/teste/viewModel/PhotoSchemeViewModel.kt
@@ -3,51 +3,53 @@ package com.br.photoscheme.teste.viewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.br.photoscheme.teste.models.PhotoItem
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.StorageReference
-import com.google.firebase.storage.ktx.storage
-import java.util.*
+import com.br.photoscheme.teste.repository.PhotoSchemeRepository
+import com.br.photoscheme.teste.service.dao.PhotoListDAO
+import com.br.photoscheme.teste.service.entity.asDomainModel
+import com.br.photoscheme.teste.state.PhotoSchemeState
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class PhotoSchemeViewModel : ViewModel() {
-    private val storage = Firebase.storage
+    private val repository = PhotoSchemeRepository()
 
-    private var mPhotoList = MutableLiveData<List<PhotoItem>>()
-    var photoList: LiveData<List<PhotoItem>> = mPhotoList
+    private var mPhotoList = MutableLiveData<PhotoSchemeState>()
+    var photoList: LiveData<PhotoSchemeState> = mPhotoList
 
     fun fetchPhotos() {
-        val list: MutableList<PhotoItem> = mutableListOf()
+        mPhotoList.value = PhotoSchemeState.Loading
 
-        storage.reference.child("thumb").listAll().addOnSuccessListener { photos ->
-            photos.items.forEachIndexed { index, storageReference ->
-                storageReference.downloadUrl.addOnSuccessListener {
-                    val photo = PhotoItem(index.toString(), it.toString())
-                    list.add(photo)
+        CoroutineScope(Dispatchers.IO).launch {
+            val photoList = async { repository.getPhotoList() }.await()
 
-                    if (index == photos.items.size - 1) {
-                        mPhotoList.value = list
-                    }
-                }.addOnFailureListener {
-                    val l = ""
+            photoList.let {
+                val parseLinks = async { repository.parseStorageReferenceToLink(photoList) }.await()
+                withContext(Dispatchers.Main) {
+                    mPhotoList.value = PhotoSchemeState.Success(parseLinks)
                 }
             }
         }
     }
 
     fun updatePhoto(downsizedImageBytes: ByteArray?) {
-        val uuid = UUID.nameUUIDFromBytes(downsizedImageBytes)
-        val storageReference = FirebaseStorage.getInstance().getReference("/thumb").child(uuid.toString())
-        storageReference.putBytes(downsizedImageBytes!!)
+        CoroutineScope(Dispatchers.IO).launch {
+            async { repository.updatePhoto(downsizedImageBytes) }.await()
+
+            withContext(Dispatchers.Main) {
+                fetchPhotos()
+            }
+        }
     }
 
-    private fun downloadPhotoUrl(photo: StorageReference): String {
-        var url = ""
-        photo.downloadUrl.addOnSuccessListener {
-            url = it.toString()
-        }.addOnFailureListener {
-            val x = ""
+    fun fetchPhotosOfDB(dao: PhotoListDAO) {
+        mPhotoList.value = PhotoSchemeState.Loading
+
+        val newPhotoList = dao.getPhotoList().map {
+            it.asDomainModel()
         }
-        return url
+        mPhotoList.value = PhotoSchemeState.Success(newPhotoList)
     }
 }
