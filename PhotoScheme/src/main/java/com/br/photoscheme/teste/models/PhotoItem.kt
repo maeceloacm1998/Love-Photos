ee111f2e02d8582219783afdce831e3232228138
marcelo.mendes <marcelo.cmendes@b2wdigital.com>
2023-01-19 01:06:59 -0300

    feat: add parser photoItem to PhotoListEntity

--- PhotoScheme/src/main/java/com/br/photoscheme/teste/models/PhotoItem.kt
+++ PhotoScheme/src/main/java/com/br/photoscheme/teste/models/PhotoItem.kt
@@ -1,6 +1,16 @@
 package com.br.photoscheme.teste.models
 
+import com.br.photoscheme.teste.service.entity.PhotoListEntity
+
 data class PhotoItem(
     val id: String,
     val url: String
 )
+
+fun PhotoItem.asDomainModel(): PhotoListEntity {
+    val photoEntity = PhotoListEntity()
+    photoEntity.mId = this.id
+    photoEntity.url = this.url
+
+    return photoEntity
+}
